a817761642c83675e388aaffe1924b7c299430b5
ssvitlana31 <130747746+ssvitlana31@users.noreply.github.com>
2023-11-21 15:54:27 +0200

    task

--- src/2.ts
+++ src/2.ts
@@ -16,15 +16,17 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  // constructor(name: string, department: string, salary: number) {
+  //   this.name = name;
+  //   this.department = department;
+  //   this.salary = salary;
+  // }
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -33,7 +35,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 1000);
+  }
 }
 
-
-export {};
+export { Manager };
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,41 @@
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean;
+  public key: Key;
+  private tenants: Person[] = [];
+  public comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new MyHouse();
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
